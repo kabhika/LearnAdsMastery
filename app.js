845013d8bd6666cbf04b602c1e0b0beb3647bdb2
Abhi <abhi1010@live.com>
2026-09-15 16:05:17 +1000

    Relay sync: bounce hub sync laps through first-party storage (iframe partitioning workaround)

--- app.js
+++ app.js
@@ -799,5 +799,11 @@ renderAll();
   };
   const orig = window.renderAll;
   if (typeof orig === "function") window.renderAll = function () { orig.apply(this, arguments); post(); };
+  const params = new URLSearchParams(location.search);
+  if (params.get("relay") === "hub") {
+    location.replace("https://learn-paths-hub.vercel.app/#p=ads-mastery:" +
+      Object.keys(state.completions).length + "/" + TASKS.length + "&i=" + (params.get("i") || "0"));
+    return;
+  }
   post();
 })();
